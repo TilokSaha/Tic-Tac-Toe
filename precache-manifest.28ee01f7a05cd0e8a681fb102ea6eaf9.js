a6f3f5c38dbea208339993deb6a7e42a6db7498e
Tilok Saha <tiloksaha60@gmail.com>
2020-02-24 18:34:43 +0530

    Updates

---
+++ precache-manifest.28ee01f7a05cd0e8a681fb102ea6eaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2b6187b5b82a83d6d9c",
+    "url": "/Tic-Tac-Toe/static/css/main.c5e2aa19.chunk.css"
+  },
+  {
+    "revision": "b2b6187b5b82a83d6d9c",
+    "url": "/Tic-Tac-Toe/static/js/main.b2b6187b.chunk.js"
+  },
+  {
+    "revision": "10779627d94960e7b271",
+    "url": "/Tic-Tac-Toe/static/js/runtime~main.10779627.js"
+  },
+  {
+    "revision": "645c1eb4e9e72a82058a",
+    "url": "/Tic-Tac-Toe/static/js/2.645c1eb4.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4bdc001afa174f8dc6afeb9a5bab939d",
+    "url": "/Tic-Tac-Toe/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Tic-Tac-Toe/precache-manifest.28ee01f7a05cd0e8a681fb102ea6eaf9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Tic-Tac-Toe/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
